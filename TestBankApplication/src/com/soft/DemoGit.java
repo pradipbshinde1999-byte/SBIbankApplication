9f39f47373fda90948c98d9f707dc1296c97cb19
pradipbshinde1999-byte <pradipbshinde1999@gmail.com>
2026-01-29 17:06:44 +0530

    Update DemoGit.java

--- TestBankApplication/src/com/soft/DemoGit.java
+++ TestBankApplication/src/com/soft/DemoGit.java
@@ -5,6 +5,7 @@ public class DemoGit {
 	public static void main(String[] args) {
 	System.out.println("this is the first git demo practice programm..");
 	System.out.println("Welcome to thr github practice,,");
+	System.out.println("the github repository pull on local repo,,");	
 
 	}
 
